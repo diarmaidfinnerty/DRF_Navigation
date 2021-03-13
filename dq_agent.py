f280bf7a66c91bdb7ba24064fbe8c7b4ffd69f0c
Diarmaid Finnerty <diarmaidfinnerty@gmail.com>
2021-03-13 11:07:59 +0000

    chore: removed unnecessary comments

--- dq_agent.py
+++ dq_agent.py
@@ -85,8 +85,6 @@ class Agent():
             gamma (float): discount factor
         """
         states, actions, rewards, next_states, dones = experiences
-        ## TODO: compute and minimize the loss
-        "*** YOUR CODE HERE ***"
         Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
         Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
         
